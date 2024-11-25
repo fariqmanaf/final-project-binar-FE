56a2df53f120559553861eadc0325d4f7f66dedf
fariqmanaf <fariqmanaf24@gmail.com>
2024-11-25 23:57:26 +0700

    fix: import assets

--- src/routes/auth/otp.jsx
+++ src/routes/auth/otp.jsx
@@ -7,7 +7,7 @@ import {
   InputOTPSlot,
 } from "@/components/ui/input-otp";
 import "./Otp.css";
-import logo from "../../assets/logo.png";
+import logo from "/logo.png";
 
 export const Route = createFileRoute("/auth/otp")({
   component: Otp,
