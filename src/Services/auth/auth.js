f28e793cec6e28db00a2ca81eff7c3b72d0d2aa8
Bhisma Haris Alfitrah <117456239+bhismaharis@users.noreply.github.com>
2024-11-27 00:08:27 +0700

    feat: register (#8)

--- src/Services/auth/auth.js
+++ src/Services/auth/auth.js
@@ -15,21 +15,19 @@ export const login = async (request) => {
   return result?.data;
 };
 
-export const register = async (request) => {
-  const formData = new FormData();
-  formData.append('name', request.name);
-  formData.append('email', request.email);
-  formData.append('phone_number', request.phone_number);
-  formData.append('password', request.password);
-
+export const register = async (data) => {
+  console.log(JSON.stringify(data));
   const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/register`, {
     method: 'POST',
-    body: formData,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
 
   const result = await response.json();
-  if (!result?.success) {
-    throw new Error(result?.message);
+  if (result.error) {
+    throw new Error(result?.error.message);
   }
 
   return result?.data;
