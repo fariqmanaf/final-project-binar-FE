3ec2989e9bf4a3c842096ddf38c1820fca2bb7c3
nabilah24 <nabilah.zakiyah.2205356@students.um.ac.id>
2024-11-29 15:43:51 +0700

    feat: create navbar components

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -1,17 +1,31 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Outlet, useLocation } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import '../index.css';
+import Navbar from '@/components/Navbar';
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <div style={{ display: 'flex' }}>
-        {/* Main Content */}
-        <div style={{ flex: 5 }}>
-          <Outlet />
+  component: () => {
+    const location = useLocation();
+    const authPaths = [
+      '/auth/login',
+      '/auth/register',
+      '/auth/otp',
+      '/auth/password-reset/$token',
+      '/auth/password-reset/verify-email',
+    ];
+
+    const isAuthRoute = authPaths.some((path) => location.pathname.startsWith(path));
+    return (
+      <>
+        <div style={{ display: 'flex' }}>
+          {/* Main Content */}
+          <div style={{ flex: 5 }}>
+            {!isAuthRoute && <Navbar />}
+            <Outlet />
+          </div>
         </div>
-      </div>
-      <TanStackRouterDevtools />
-    </>
-  ),
+        <TanStackRouterDevtools />
+      </>
+    );
+  },
 });
