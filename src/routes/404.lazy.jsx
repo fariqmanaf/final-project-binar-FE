2008fd6993456b4cb4334e3e543790479ffd4860
arswendoerz <arswendoerza00@gmail.com>
2024-12-13 18:05:46 +0700

    feat: add slicing page notification, account & 404

--- src/routes/404.lazy.jsx
+++ src/routes/404.lazy.jsx
@@ -1,14 +1,35 @@
 import * as React from 'react';
-import { createLazyFileRoute } from '@tanstack/react-router';
+import { createLazyFileRoute, useNavigate } from '@tanstack/react-router';
+import { Button } from '@/components/ui/button';
 
 export const Route = createLazyFileRoute('/404')({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const navigate = useNavigate();
+
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <h1 className="text-4xl font-bold text-gray-800">404 Not Found</h1>
+    <div className="font-poppins container mx-auto w-[90%] md:w-[80%] px-4 py-8">
+      <div className="flex flex-col md:flex-row items-center justify-center text-center md:text-left p-4">
+        <img src="/notfound.svg" alt="Tidak Ditemukan" className="mb-4 md:mb-0 md:mr-8 w-64 md:w-auto" />
+        <div>
+          <h1 className="text-4xl font-bold mb-4">
+            Oops! <br /> Halaman Tidak Ditemukan
+          </h1>
+          <p className="text-lg mb-4 text-justify">
+            Halaman yang Anda cari mungkin telah dihapus, namanya diubah, atau sementara tidak tersedia. Kami mohon maaf
+            atas ketidaknyamanan ini. Silakan periksa URL Anda atau gunakan tombol di bawah ini untuk kembali ke
+            beranda.
+          </p>
+          <Button
+            className="bg-[#A06ECE] mt-[5px] hover:bg-[#4B1979] text-white w-[150px] h-[48px] rounded-xl shadow-lg transition duration-300 ease-in-out transform hover:scale-105"
+            onClick={() => navigate({ to: `/` })}
+          >
+            Kembali ke Beranda
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
