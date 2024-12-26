21f6c8f5919245f0bea1a7cf186782fa6c4e2669
fariqmanaf <fariqmanaf24@gmail.com>
2024-12-27 02:15:18 +0700

    fix: button load more in history

--- src/routes/history/index.lazy.jsx
+++ src/routes/history/index.lazy.jsx
@@ -189,16 +189,15 @@ function History() {
                   className="flex flex-col mt-[5vh] gap-[5vh]"
                 >
                   <HistoryItem data={history} className="w-full" active={active} setActive={handleHistoryItemClick} />
-                  {!isLoading ||
-                    (history.length === 0 && (
-                      <button
-                        onClick={() => fetchNextPage()}
-                        disabled={!hasNextPage || isFetchingNextPage}
-                        className="text-md pb-[3vh] -mt-10 text-[#7126B5] font-semibold"
-                      >
-                        {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
-                      </button>
-                    ))}
+                  {history.length > 0 && (
+                    <button
+                      onClick={() => fetchNextPage()}
+                      disabled={!hasNextPage || isFetchingNextPage}
+                      className="text-md pb-[3vh] -mt-10 text-[#7126B5] font-semibold"
+                    >
+                      {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
+                    </button>
+                  )}
                 </motion.div>
               ) : (
                 <div className="flex flex-col gap-4 mt-[5vh] mb-5">
@@ -243,16 +242,15 @@ function History() {
                 className="flex flex-col md:w-[60%] w-[100%] gap-[5vh]"
               >
                 <HistoryItem data={history} className="w-full" active={active} setActive={setActive} />
-                {!isLoading ||
-                  (history.length === 0 && (
-                    <button
-                      onClick={() => fetchNextPage()}
-                      disabled={!hasNextPage || isFetchingNextPage}
-                      className="text-md pb-[3vh] -mt-10 text-[#7126B5] font-semibold"
-                    >
-                      {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
-                    </button>
-                  ))}
+                {history.length > 0 && (
+                  <button
+                    onClick={() => fetchNextPage()}
+                    disabled={!hasNextPage || isFetchingNextPage}
+                    className="text-md pb-[3vh] -mt-10 text-[#7126B5] font-semibold"
+                  >
+                    {isFetchingNextPage ? 'Loading more...' : hasNextPage ? 'Load More' : 'Nothing more to load'}
+                  </button>
+                )}
               </motion.div>
               {history.length > 0 && (
                 <div className="flex flex-col w-[40%] gap-[2vh]">
